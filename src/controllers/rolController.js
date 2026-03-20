1f3aadd740ec290d24aedde8207cc98d7186dcd8
Sanjay <20241003@uthh.edu.mx>
2026-03-20 02:55:33 -0600

    ...

--- src/controllers/rolController.js
+++ src/controllers/rolController.js
@@ -2,9 +2,15 @@ import * as model from '../models/rolModel.js';
 
 export const obtenerRol = async (req, res) => {
     try {
-        const data = await model.getRol();
+        const data = await model.getAllRoles(); 
+        
+        if (!data || data.length === 0) {
+            return res.status(404).json({ message: 'No se encontraron roles' });
+        }
+        
         res.json(data);
     } catch (error) {
+        console.error("Error en obtenerRol:", error);
         res.status(500).json({ error: 'Error al obtener roles' });
     }
 };
