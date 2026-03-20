a59a62b472e07e241c5da24c0c17fd12c385b02c
Sanjay <20241003@uthh.edu.mx>
2026-03-20 00:22:05 -0600

    ...

--- app.js
+++ app.js
@@ -18,11 +18,7 @@ import loginRoutes from './src/routes/loginRoute.js';
 
 const app = express();
 
-app.use(cors({
-  origin:      process.env.FRONTEND_URL, // lee del .env
-  methods:     ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
