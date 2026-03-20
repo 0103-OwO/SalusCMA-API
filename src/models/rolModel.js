cd9b2d1732075e8de40863810d29e119c3967756
Sanjay <20241003@uthh.edu.mx>
2026-03-20 02:50:23 -0600

    ...

--- src/models/rolModel.js
+++ src/models/rolModel.js
@@ -1,6 +1,6 @@
 import db from '../config/db.js';
 
-export const getRol = async () => {
-    const [rows] = await db.query('SELECT * FROM rol');
-    return rows[0];
+export const getAllRoles = async () => {
+  const [rows] = await db.query('SELECT * FROM rol');
+  return rows;
 };
