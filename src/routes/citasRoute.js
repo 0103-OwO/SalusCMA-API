549b84e428301dd16cd7e44c085daea93a0ed189
Sanjay <20241003@uthh.edu.mx>
2026-03-23 20:14:19 -0600

    ..

--- src/routes/citasRoute.js
+++ src/routes/citasRoute.js
@@ -4,10 +4,11 @@ import { verificarToken } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
+router.get('/mis-citas', verificarToken, ctrl.getCitasMedico);
 router.get('/', verificarToken, ctrl.obtenerCitas);
 router.get('/:id', verificarToken, ctrl.getCita);
 router.post('/', verificarToken, ctrl.createCita);
 router.put('/:id', verificarToken, ctrl.updateCita);
 router.delete('/:id', verificarToken, ctrl.deleteCita);
-router.get('/mis-citas', verificarToken, ctrl.getCitasMedico);
+
 export default router;
