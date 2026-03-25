7149860defe207044224957c043b1781d981b66f
Sanjay <20241003@uthh.edu.mx>
2026-03-25 01:18:05 -0600

    ..

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -1,8 +1,16 @@
 import db from '../config/db.js';
 
 export const buscarUsuarioGlobal = async (identificador) => {
-    const [result] = await db.query('CALL sp_login_usuario(?)', [identificador]);
-    return result[0][0]; 
+    try {
+        const [result] = await db.query('CALL sp_login_usuario(?)', [identificador]);
+        if (result && result[0] && result[0].length > 0) {
+            return result[0][0];
+        }
+        return null;
+    } catch (error) {
+        console.error("Error en buscarUsuarioGlobal:", error.message);
+        throw error;
+    }
 };
 
 export const getNombrePaciente = async (id) => {
