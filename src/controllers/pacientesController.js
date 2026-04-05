04610ec997a2f66b02cb9cfb8059fa298239361f
Sanjay <20241003@uthh.edu.mx>
2026-04-05 16:48:50 -0600

    ...

--- src/controllers/pacientesController.js
+++ src/controllers/pacientesController.js
@@ -36,7 +36,17 @@ export const listarPacientesActivos = async (req, res) => {
 
 export const createPaciente = async (req, res) => {
   try {
-    const { curp, usuario, contrasena } = req.body;
+    const {
+      curp,
+      nombre,
+      app,
+      apm,
+      sexo,
+      fecha_nac,
+      correo,
+      usuario,
+      contrasena
+    } = req.body;
 
     if (await model.checkCurpExists(curp)) {
       return res.status(400).json({ msg: "La CURP ya existe." });
